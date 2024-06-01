b5224ac01466697dc1fa23c7e403a7a3ae73d5fc
frangmarquez <fcoj.mar.gon@gmail.com>
2024-06-01 14:47:18 +0200

    Basis-related algorithms to implement

---
+++ basis_reduction/basisTransformer.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class BasisTransformer():
+
+    def __init__(self):
+        pass
+
+    def isIndependent(self, basis):
+        pass
+
+    def orthogonalize_basis(self, basis):
+        pass
+
+    def reduce_basis(self, basis, method='LLL'):
+        pass
